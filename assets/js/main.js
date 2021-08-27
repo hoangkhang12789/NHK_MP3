87d1f76c25de81e6d145ca269ef652292e622545
Nguyễn Hoàng Khang <hoangkhang12789@gmail.com>
2021-08-27 22:10:07 +0700

    add music player

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,31 +15,65 @@ tabs.forEach((tab, index) => {
 })
 
 
-// Render music
-const audio =$("#audio");
-const app = {
-    song:[
-    {
-        title: "Níu Duyên",
-        Singer: "Lê Bảo Bình",
-        path: "./assets/music/NiuDuyen-LeBaoBinh-6872127.mp3",
-        image: "./assets/image/LeBaoBinh.jpg"
-    },
-     {
-        title: "Phận Duyên Lỡ Làng",
-        Singer: "PHÁT HUY T4 X TRUZG",
-        path: "./assets/music/PhanDuyenLoLang-PhatHuyT4Trugz-7004538.mp3",
-        image: "./assets/image/PHATHUYT4XTRUZG.jpg"
-    }, 
-    {
-        title: "Sài Gòn đau Lòng Qúa",
-        Singer: "Lê Bảo Bình",
-        path: "./assets/music/SaiGonDauLongQua-HuaKimTuyenHoangDuyen-6992977.mp3",
-        image: "./assets/image/HuaKimTuyen.jpg"
-    }
 
-    ]
+
+// Render music
+const audio = $("#audio");
+const play = $(".play-btn");
+const pause = $(".pause");
+const playAndPause = $(".button .active");
+const app = {
+    song: [
+        {
+            title: "Níu Duyên",
+            Singer: "Lê Bảo Bình",
+            path: "./assets/music/NiuDuyen-LeBaoBinh-6872127.mp3",
+            image: "./assets/image/LeBaoBinh.jpg"
+        },
+        {
+            title: "Phận Duyên Lỡ Làng",
+            Singer: "PHÁT HUY T4 X TRUZG",
+            path: "./assets/music/PhanDuyenLoLang-PhatHuyT4Trugz-7004538.mp3",
+            image: "./assets/image/PHATHUYT4XTRUZG.jpg"
+        },
+        {
+            title: "Sài Gòn đau Lòng Qúa",
+            Singer: "Lê Bảo Bình",
+            path: "./assets/music/SaiGonDauLongQua-HuaKimTuyenHoangDuyen-6992977.mp3",
+            image: "./assets/image/HuaKimTuyen.jpg"
+        }
+
+    ],
+    handledSong: function () {
+        console.log("56666")
+    },
+    playSong: function () {
+        play.onclick = () => {
+            playAndPause.classList.remove("active");
+            audio.play();
+            pause.classList.add("active");
+            
+
+        }
+
+    },
+    pauseSong: function () {
+        pause.onclick = () => {
+            pause.classList.remove("active");
+            audio.pause();
+            play.classList.add('active');
+            
+
+        }
+    }
+    ,
+    run: function () {
+        this.handledSong();
+        this.playSong();
+        this.pauseSong();
+    }
 }
+app.run();
 
 
 
