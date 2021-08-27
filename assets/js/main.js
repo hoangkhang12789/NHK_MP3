62f6cf51006b8b7925ce73fd7f0426990250c0fb
Nguyễn Hoàng Khang <hoangkhang12789@gmail.com>
2021-08-27 19:57:51 +0700

    add music player

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 "use strick"
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-
+// Chuyển tab
 const tabs = $$("#nav li");
 const contents = $$(".content div");
 tabs.forEach((tab, index) => {
@@ -13,6 +13,34 @@ tabs.forEach((tab, index) => {
         content.classList.add('active');
     }
 })
+
+
+// Render music
+const audio =$("#audio");
+const app = {
+    song:[
+    {
+        title: "Níu Duyên",
+        Singer: "Lê Bảo Bình",
+        path: "./assets/music/NiuDuyen-LeBaoBinh-6872127.mp3",
+        image: "./assets/image/LeBaoBinh.jpg"
+    },
+     {
+        title: "Phận Duyên Lỡ Làng",
+        Singer: "PHÁT HUY T4 X TRUZG",
+        path: "./assets/music/PhanDuyenLoLang-PhatHuyT4Trugz-7004538.mp3",
+        image: "./assets/image/PHATHUYT4XTRUZG.jpg"
+    }, 
+    {
+        title: "Sài Gòn đau Lòng Qúa",
+        Singer: "Lê Bảo Bình",
+        path: "./assets/music/SaiGonDauLongQua-HuaKimTuyenHoangDuyen-6992977.mp3",
+        image: "./assets/image/HuaKimTuyen.jpg"
+    }
+
+    ]
+}
+
 
 
 
